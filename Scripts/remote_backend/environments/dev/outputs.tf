d15e37818563704503e91f8fbafafc465b0724e0
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-11-13 17:43:31 +0000

    Add. private_dns output

--- Scripts/remote_backend/environments/dev/outputs.tf
+++ Scripts/remote_backend/environments/dev/outputs.tf
@@ -8,3 +8,7 @@ output "aws_ec2_private_ip" {
 output "aws_ec2_public_dns" {
   value = aws_instance.dev-server.public_dns
 }
+
+output "aws_ec2_private_dns" {
+  value = aws_instance.dev-server.private_dns
+}
